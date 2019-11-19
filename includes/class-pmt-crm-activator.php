9faeb320689b386a7cbab0b9132edbff85704540
Zorca Orcinus <vs@zorca.org>
2019-11-19 20:44:22 +0300

    Init

---
+++ includes/class-pmt-crm-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://zorca.org
+ * @since      1.0.0
+ *
+ * @package    Pmt_Crm
+ * @subpackage Pmt_Crm/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Pmt_Crm
+ * @subpackage Pmt_Crm/includes
+ * @author     Zorca Orcinus <vs@zorca.org>
+ */
+class Pmt_Crm_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-pmt-crm-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://zorca.org
+ * @since      1.0.0
+ *
+ * @package    Pmt_Crm
+ * @subpackage Pmt_Crm/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Pmt_Crm
+ * @subpackage Pmt_Crm/includes
+ * @author     Zorca Orcinus <vs@zorca.org>
+ */
+class Pmt_Crm_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
